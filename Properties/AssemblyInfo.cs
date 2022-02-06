5c04c5534864936dd826f8503d98cd8299353632
bertskyksu <bertsky@ksu.edu>
2022-02-06 14:41:12 -0600

    added required files to github repo

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LORICA Landscape Evolution Model")]
+[assembly: AssemblyDescription("The LORICA project was started in the early 2000's by Jeroen Schoorl.\n\nTeam: Arnaud Temme, Jeroen Schoorl, Lieven Claessens, Wouter van Gorp, Jantiene Baartman\n\nSpecial thanks to Tom Coulthard for the coding template: www.coulthard.org.uk\n\nThis program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; Without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/copyleft/gpl.html) for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wageningen University, NL")]
+[assembly: AssemblyProduct("LORICA Landscape Evolution Model")]
+[assembly: AssemblyCopyright("Copyright © WUR - LAD - Arnaud Temme 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("badc150f-24e5-4be1-a645-53d65e5d5118")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("4.005")]
+[assembly: AssemblyFileVersion("october 2010")]
