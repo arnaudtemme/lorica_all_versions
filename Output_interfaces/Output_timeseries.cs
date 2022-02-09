2a50e1e5a70189f44d232fc5199a25639b2f33d3
Arnaud J Temme <arnaudtemme@ksu.edu>
2022-02-09 14:17:51 -0600

    Adding missing files and Mathnet.numerics package

---
+++ Output_interfaces/Output_timeseries.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LORICA4
+{
+    public partial class Output_timeseries : Form
+    {
+        public double timeseries_waterflow_threshold = 0;
+        public double timeseries_deposition_threshold = 0;
+        public double timeseries_erosion_threshold = 0;
+
+        public Output_timeseries()
+        {
+            InitializeComponent();
+        }
+
+        private void Timeseries_output_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(timeseries_number_waterflow_check.Checked){
+                try { timeseries_waterflow_threshold = System.Convert.ToDouble(timeseries_textbox_waterflow_threshold.Text); }
+                catch { MessageBox.Show("invalid waterflow threshold"); }
+                if (timeseries_waterflow_threshold < 0) { MessageBox.Show("invalid waterflow threshold"); }
+            }
+            if (timeseries_number_dep_check.Checked)
+            {
+                try { timeseries_deposition_threshold = System.Convert.ToDouble(timeseries_textbox_deposition_threshold.Text); }
+                catch { MessageBox.Show("invalid deposition threshold"); }
+                if (timeseries_deposition_threshold < 0) { MessageBox.Show("invalid deposition threshold"); }
+            }
+            if (timeseries_number_erosion_check.Checked)
+            {
+                try { timeseries_erosion_threshold = System.Convert.ToDouble(timeseries_textbox_erosion_threshold.Text); }
+                catch { MessageBox.Show("invalid erosion threshold"); }
+                if (timeseries_erosion_threshold < 0) { MessageBox.Show("invalid erosion threshold"); }
+            }
+            this.Visible = false;
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LORICA Soilscape Evolution Model")]
+[assembly: AssemblyDescription("The LORICA project was started in 2015 by Arnaud Temme and Tom Vanwalleghem.\n\nTeam: Arnaud Temme, Marijn van der Meij, Tom Vanwalleghem\n\nThis program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; Without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License (http://www.gnu.org/copyleft/gpl.html) for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Hydro-, Chrono-, LithoLORICA")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("badc150f-24e5-4be1-a645-53d65e5d5118")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("January 2022")]
